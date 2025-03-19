f246c0d5485afa07ec815021401ceca5e46f9fb8
Spegelcode <leo.spegel.berg@gmail.com>
2025-03-19 08:08:41 +0100

    Lektion1 = done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Lektion1Component } from "./lektion1/lektion1.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Lektion1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/lektion1/lektion1.component.ts
+++ src/app/lektion1/lektion1.component.ts
@@ -5,7 +5,58 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './lektion1.component.html',
   styleUrl: './lektion1.component.css'
-})
+}) 
+  export interface User {
+    name: string;
+    age: number;
+  }
+
+  export interface Item {
+    name: string;
+    value: number;
+  }
+  
+
+
 export class Lektion1Component {
+  
+  public width: number = 100;
+  public height: number = 100;
+  public items: Item[] = [
+    {name: 'item1', value: 1},
+    {name: 'item2', value: 2},
+    {name: 'item3', value: 3}
+  ];
+
+  public calculateArea(width: number, height: number): number {
+    return width*height;
+  }
+  public createUser(name: string, age: number): {name: string; age: number} {
+    return { name: name, age: age};
+  }
+  
+
+  public createUser2(name: string, age: number): User {
+    return { name: name, age: age};
+  }
+
+  public addItems(items: Item[], item: Item): Item[] {
+    items.push(item);
+    return items;
+  }
+
+
+  public handelResponse(statusCode: HttpStatusCode) {
+    if (statusCode === StatusCode.Success) {
+      console.log('Status OK');
+    } else if (statusCode === StatusCode.Notfound) {
+      console.log('Status Error');
 
 }
+
+export enum HttpStatusCode {
+  Success = 200,
+  Notfound = 404
+}
+
+
